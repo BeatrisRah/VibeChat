7b8a1deb01ad15be841322351358675847f1f19a
Beatris <beatrisraj7@gmail.com>
2025-02-21 17:15:08 +0200

    Env set up

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import express from 'express'
+import 'dotenv/config';
 
 const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    
+    res.send('Works')
+})
+
+
+
+app.listen(port, () => console.log(`App is listening at http://localhost:${port}...` ))
