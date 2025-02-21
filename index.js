e297129c66463e732a0cd73a526c5397722d35cb
Beatris <beatrisraj7@gmail.com>
2025-02-21 17:29:51 +0200

    Handlebars set up

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from 'express'
 import 'dotenv/config';
 import router from './routes.js';
+import handlebars from 'express-handlebars';
 
 const app = express()
 const port = process.env.PORT || 3000;
 
+//express engine
+app.engine('handlebars', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'handlebars')
+app.set('views', '/src/views')
+
+
+//Routing
 app.use(router)
 
 
