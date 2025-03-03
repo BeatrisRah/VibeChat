ab86bc0404cd802b2e6461a4f367eee6ba0936b1
Beatris <beatrisraj7@gmail.com>
2025-03-03 18:04:06 +0200

    Added register post req

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
+import authService from "../services/auth-service.js";
 const authRouter = Router()
 
 authRouter.get('/register', (req, res) => {
     res.render('register')
+})
+
+authRouter.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    try{
+        await authService.registerUSer(userData)
+        
+
+    } catch(err){
+        console.log(err.message);
+        
+    }
+    res.redirect('/')
 })
 
 authRouter.get('/login', (req, res) => {
