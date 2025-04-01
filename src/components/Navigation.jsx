b86e95b253b5fe1006c29d7a2fb81bde8fe92dd9
Beatris <beatrisraj7@gmail.com>
2025-04-01 21:06:31 +0300

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,23 @@
 export default function Navigation() {
    return (
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar fixed top-0 left-0 w-full px-4" 
+         style={{
+         backgroundColor: "rgba(17, 24, 39, 0)", 
+         backdropFilter: "blur(10px)",
+      }}>
          <div className="flex-1">
             <a className="btn btn-ghost text-xl">VibeChat</a>
          </div>
          <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
-               <li><a>Link</a></li>
+               <li><a>Home</a></li>
+               <li><a>About</a></li>
                <li>
                   <details>
-                     <summary>Parent</summary>
-                     <ul className="bg-base-100 rounded-t-none p-2">
-                        <li><a>Link 1</a></li>
-                        <li><a>Link 2</a></li>
+                     <summary>More</summary>
+                     <ul className="bg-base-100 bg-opacity-80 rounded-t-none p-2">
+                        <li><a>Contact</a></li>
+                        <li><a>Help</a></li>
                      </ul>
                   </details>
                </li>
