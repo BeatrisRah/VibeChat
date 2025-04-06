248a35d9eb37dc8eebfa14ced2901a0e3c05c2c3
Beatris <beatrisraj7@gmail.com>
2025-04-06 21:54:03 +0300

    React hook form for auth form

--- src/pages/auth/AuthForm.jsx
+++ src/pages/auth/AuthForm.jsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form"
 
 export default function AuthForm() {
     const [tab, setTab] = useState("login");
+    const {register, handleSubmit, formState: { errors }, watch} = useForm()
+
+    const password = watch('password')
+
+    const onSubmit = (data) => {
+        if (tab === "login") {
+          
+          console.log("Logging in with:", data);
+        } else if (tab === "register") {
+         
+          console.log("Registering with:", data);
+        }
+      };
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-base-200">
@@ -24,44 +38,84 @@ export default function AuthForm() {
                 </div>
 
                 {tab === "login" && (
-                    <form className="space-y-6">
+                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
                         <h2 className="text-xl font-bold text-center">Login</h2>
                         <div className="form-control w-11/12 mx-auto space-y-2">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email@example.com" className="input input-bordered w-full" required />
+                            <input 
+                                type="email"
+                                {...register("email", { required: "Email is required" })} 
+                                placeholder="email@example.com" 
+                                className="input input-bordered w-full" 
+                                />
+                                {errors.email && <p className="text-red-500">{errors.email.message}</p>}
                         </div>
                         <div className="form-control w-11/12 mx-auto space-y-2">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered w-full" required />
+                            <input 
+                                type="password" 
+                                {...register("password", { required: "Password is required" })}
+                                placeholder="password" 
+                                className="input input-bordered w-full" />
+                                {errors.password && <p className="text-red-500">{errors.password.message}</p>}
                         </div>
                         <button type="submit" className="btn btn-primary w-full">Login</button>
                     </form>
                 )}
 
                 {tab === "register" && (
-                    <form className="space-y-6">
+                    <form  onSubmit={handleSubmit(onSubmit)} className="space-y-6">
                         <h2 className="text-xl font-bold text-center">Register</h2>
                         <div className="form-control w-11/12 mx-auto space-y-2">
                             <label className="label">
                                 <span className="label-text">Username</span>
                             </label>
-                            <input type="text" placeholder="Your username" className="input input-bordered w-full" required />
+                            <input 
+                            type="text" 
+                            {...register("username", { required: "Username is required" })}
+                            placeholder="Your username" 
+                            className="input input-bordered w-full" 
+                            />
+                            {errors.username && <p className="text-red-500">{errors.username.message}</p>}
                         </div>
                         <div className="form-control w-11/12 mx-auto space-y-2">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email@example.com" className="input input-bordered w-full" required />
+                            <input 
+                                type="email" 
+                                {...register("email", { required: "Email is required" })}
+                                placeholder="email@example.com" 
+                                className="input input-bordered w-full"/>
+                                {errors.email && <p className="text-red-500">{errors.email.message}</p>}
                         </div>
                         <div className="form-control w-11/12 mx-auto space-y-2">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered w-full" required />
+                            <input 
+                                type="password" 
+                                {...register("password", { required: "Password is required" })}
+                                placeholder="password" 
+                                className="input input-bordered w-full"/>
+                                {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+                        </div>
+                        <div className="form-control w-11/12 mx-auto space-y-2">
+                            <label className="label">
+                                <span className="label-text">Confirm Password</span>
+                            </label>
+                            <input 
+                                type="password" 
+                                {...register("rePass", { 
+                                    required: "Confirm Password is required",
+                                    validate: value => value === password || 'Passwords do not match'  })}
+                                placeholder="Confirm Password" 
+                                className="input input-bordered w-full"/>
+                                {errors.rePass && <p className="text-red-500">{errors.rePass.message}</p>}
                         </div>
                         <button type="submit" className="btn btn-secondary w-full">Register</button>
                     </form>
